7a07fd34b608373e04ea756c95e378521699bc4f
ashwithaDevireddy <devireddyashwitha@gmail.com>
2022-09-18 23:31:34 -0600

    Objects

---
+++ Objects/obj.java
@@ -0,0 +1,11 @@
+package Objects;
+
+public class obj {
+    int size;
+    String name;
+    String brand;
+
+    void bark(){
+        System.out.println("Ruff! Reuff!");
+    }
+}
---
+++ Objects/objTest.java
@@ -0,0 +1,12 @@
+package Objects;
+
+public class objTest {
+    public static void main(String[] Args){
+        // new Object creation
+        obj d = new obj();
+        //Using the object variable to set the value
+        d.size=20;
+        //calling the bark method specified in obj class by using a dot operator
+        d.bark();
+    }
+}
